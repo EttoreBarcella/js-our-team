602b632296077c9f64bca95493477d1e94564402
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-05-13 15:15:39 +0200

    added object

---
+++ main.js
@@ -0,0 +1,39 @@
+// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html
+// stampare dinamicamente una card per ogni membro del team.
+
+// Creo oggetto comune Card
+
+const card = [
+    {
+        id : "Wayne Barnett",
+        role : "Founder & CEO",
+        proPic : "img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        id : "Angela Caroll",
+        role : "Chief Editor",
+        proPic : "img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        id : "Angela Lopez",
+        role : "Social Media Manager",
+        proPic : "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        id : "Barbara Ramos",
+        role : "Graphic Designer",
+        proPic : "img/barbara-ramos-graphic-designer.jpg",
+    },
+    {
+        id : "Scott Estrada",
+        role : "Developer",
+        proPic : "img/scott-estrada-developer.jpg",
+    },
+    {
+        id : "Walter Gordon",
+        role : "Office Manager",
+        proPic : "img/walter-gordon-office-manager.jpg",
+    },
+]
